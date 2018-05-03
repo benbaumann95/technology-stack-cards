850ee0dfa37c3e5afa8e5cbc13ef53d29e167485
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-05-03 10:44:36 +0100

    Library List

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { FlatList } from 'react-native';
+import ListItem from './ListItem';
 
-class LibraryList extends React.Component {
+class LibraryList extends Component {
+  renderItem({ item }) {
+    return <ListItem item={item} />;
+  }
+
   render() {
-    console.log(this.props);
-    return;
+    return (
+      <FlatList
+        data={this.props.libraries}
+        renderItem={this.renderItem}
+        keyExtractor={item => item.id.toString()}
+      />
+    );
   }
 }
 
